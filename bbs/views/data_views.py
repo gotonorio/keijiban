40df973a8e317d333c12b5fe2b3583a7921e29ca
Goto Norio <gotonorio@gmail.com>
2022-01-12 21:52:23 +0900

    画像回転処理を追加

--- bbs/views/data_views.py
+++ bbs/views/data_views.py
@@ -3,6 +3,7 @@ from bbs.models import File
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
+from PIL import Image
 
 
 class FileCreateView(PermissionRequiredMixin, generic.CreateView):
@@ -43,4 +44,31 @@ class FileListView(PermissionRequiredMixin, generic.ListView):
     model = File
     template_name = "bbs/file_list.html"
     permission_required = ("bbs.add_file")
-    queryset = File.objects.all().order_by('-created_at')
+
+    def get_queryset(self, **kwargs):
+        qs = super().get_queryset(**kwargs)
+        qs = qs.filter(alive=True).order_by('created_at')
+        return qs
+
+
+class FileRotateView(PermissionRequiredMixin, generic.TemplateView):
+    """ 写真の回転処理
+    イメージを読み込んで、時計回りに90度回転して保存する。
+    """
+    permission_required = ("bbs.add_file")
+    template_name = "bbs/file_rotate.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        pk = self.kwargs['pk']
+        # imageのurlを求める
+        qs = File.objects.get(pk=pk)
+        img_url = qs.img.url
+        # ToDo:行頭の/が余計？行頭の1文字を削除すると問題ないようだ。
+        img_url = img_url[1:]
+        img = Image.open(img_url)
+        img_rotate = img.rotate(-90, expand=True)
+        img_rotate.save(img_url)
+        qs = File.objects.get(pk=pk)
+        context['img'] = qs.img.url
+        return context
