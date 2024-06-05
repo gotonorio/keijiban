dbec045ebb56802e2303a2a71d577848e6b0f355
gotonorio <gotonorio@gmail.com>
2024-06-05 14:54:11 +0900

    写真データからexifを削除する処理を追加

--- bbs/models.py
+++ bbs/models.py
@@ -1,16 +1,19 @@
 import os
+from io import BytesIO
 
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
+from django.core.files.base import ContentFile
 from django.db import models
 from django.utils import timezone
+from PIL import Image
 
 user = get_user_model()
 
 
 def get_upload_to(instance, filename):
-    """ upload_toを動的(カテゴリのpath毎)に指定する
+    """upload_toを動的(カテゴリのpath毎)に指定する
     https://docs.djangoproject.com/ja/4.0/ref/models/fields/
     ここで、ファイルをuploadするパスを設定する。
     settings.pyで設定したMEDIA_ROOT + IMAGE_PATH + filenameとなる。
@@ -19,28 +22,60 @@ def get_upload_to(instance, filename):
         path = os.path.join(settings.IMAGE_PATH, filename)
     except Exception as e:
         _ = e
-        path = os.path.join('default', filename)
+        path = os.path.join("default", filename)
     return path
 
 
 def image_size(value):
-    """ アップロード写真のサイズを制限 """
+    """アップロード写真のサイズを制限"""
     limit = settings.LIMMIT_IMAGE_SIZE
     if value.size > limit:
-        mb = limit/1024/1024
-        raise ValidationError(f'ファイルサイズは {mb}MB 以下にしてください。')
+        mb = limit / 1024 / 1024
+        raise ValidationError(f"ファイルサイズは {mb}MB 以下にしてください。")
+
+
+def remove_exif(image_field):
+    """exifデータを削除する"""
+    image = Image.open(image_field)
+
+    # 画像データを取得
+    data = list(image.getdata())
+    image_without_exif = Image.new(image.mode, image.size)
+    image_without_exif.putdata(data)
+
+    # バッファに新しい画像を保存
+    buffer = BytesIO()
+    image_without_exif.save(buffer, format=image.format)
+    buffer.seek(0)
+
+    # ContentFileに変換して新しいファイルを作成
+    return ContentFile(buffer.read())
 
 
 class File(models.Model):
-    """ アップロードするイメージファイル """
-    title = models.CharField(verbose_name='タイトル', max_length=32)
-    summary = models.CharField(verbose_name='概要', max_length=128, blank=True, null=True)
-    img = models.ImageField(verbose_name='写真ファイル', upload_to=get_upload_to,
-                            validators=[image_size], null=True, blank=True)
-    rank = models.IntegerField(verbose_name='表示順', default=0)
-    user = models.ForeignKey(user, verbose_name='名前', on_delete=models.CASCADE)
-    created_at = models.DateTimeField(verbose_name='作成日', default=timezone.now)
-    alive = models.BooleanField(verbose_name='有効', default=True)
+    """アップロードするイメージファイル"""
+
+    title = models.CharField(verbose_name="タイトル", max_length=32)
+    summary = models.CharField(verbose_name="概要", max_length=128, blank=True, null=True)
+    img = models.ImageField(
+        verbose_name="写真ファイル", upload_to=get_upload_to, validators=[image_size], null=True, blank=True
+    )
+    rank = models.IntegerField(verbose_name="表示順", default=0)
+    user = models.ForeignKey(user, verbose_name="名前", on_delete=models.CASCADE)
+    created_at = models.DateTimeField(verbose_name="作成日", default=timezone.now)
+    alive = models.BooleanField(verbose_name="有効", default=True)
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        """exifデータ消去"""
+        if self.img:
+            # EXIFデータを削除した画像を取得
+            new_image = remove_exif(self.img)
+
+            # 画像フィールドを新しいファイルに置き換える
+            self.img.save(self.img.name, new_image, save=False)
+
+        # 通常の保存を実行
+        super().save(*args, **kwargs)
--- bbs/views/data_views.py
+++ bbs/views/data_views.py
@@ -1,20 +1,22 @@
-from bbs.forms import FileForm
-from bbs.models import File
 from django.contrib.auth.mixins import PermissionRequiredMixin
 from django.urls import reverse_lazy
 from django.views import generic
 from PIL import Image
 
+from bbs.forms import FileForm
+from bbs.models import File
+
 
 class FileCreateView(PermissionRequiredMixin, generic.CreateView):
-    """ 管理者用 ファイルの作成. """
+    """管理者用 ファイルの作成."""
+
     model = File
     form_class = FileForm
     # 必要な権限
-    permission_required = ("bbs.add_file")
+    permission_required = "bbs.add_file"
     # 権限がない場合、Forbidden 403を返す。これがない場合はログイン画面に飛ばす。
     raise_exception = True
-    success_url = reverse_lazy('bbs:file_list')
+    success_url = reverse_lazy("bbs:file_list")
 
     def form_valid(self, form):
         # commitを停止する。
@@ -27,14 +29,15 @@ class FileCreateView(PermissionRequiredMixin, generic.CreateView):
 
 
 class FileUpdateView(PermissionRequiredMixin, generic.UpdateView):
-    """ 管理者用 ファイルの更新. """
+    """管理者用 ファイルの更新."""
+
     model = File
     form_class = FileForm
     # 必要な権限
-    permission_required = ("bbs.add_file")
+    permission_required = "bbs.add_file"
     # 権限がない場合、Forbidden 403を返す。これがない場合はログイン画面に飛ばす。
     raise_exception = True
-    success_url = reverse_lazy('bbs:file_list')
+    success_url = reverse_lazy("bbs:file_list")
 
     def form_valid(self, form):
         # commitを停止する。
@@ -47,41 +50,44 @@ class FileUpdateView(PermissionRequiredMixin, generic.UpdateView):
 
 
 class FileDeleteView(PermissionRequiredMixin, generic.DeleteView):
-    """ 管理者用 ファイルの削除. """
-    template_name = 'bbs/file_confirm_delete.html'
+    """管理者用 ファイルの削除."""
+
+    template_name = "bbs/file_confirm_delete.html"
     model = File
     # 必要な権限
-    permission_required = ("bbs.add_file")
+    permission_required = "bbs.add_file"
     # 権限がない場合、Forbidden 403を返す。これがない場合はログイン画面に飛ばす。
     raise_exception = True
-    success_url = reverse_lazy('bbs:file_list')
+    success_url = reverse_lazy("bbs:file_list")
 
 
 class FileListView(PermissionRequiredMixin, generic.ListView):
-    """ 管理者用の画像list一覧 """
+    """管理者用の画像list一覧"""
+
     model = File
     template_name = "bbs/management_list.html"
-    permission_required = ("bbs.add_file")
+    permission_required = "bbs.add_file"
 
     def get_queryset(self, **kwargs):
         qs = super().get_queryset(**kwargs)
-        qs = qs.order_by('-created_at')
+        qs = qs.order_by("-created_at")
         return qs
 
 
 class FileRotateView(PermissionRequiredMixin, generic.TemplateView):
-    """ 写真の回転処理
+    """写真の回転処理
     iPhoneで写真が横向きになるため、修正の処理。
     イメージを読み込んで、時計回りに90度回転して保存する。
     https://docs.djangoproject.com/ja/4.0/topics/files/
     """
-    permission_required = ("bbs.add_file")
+
+    permission_required = "bbs.add_file"
     template_name = "bbs/file_rotate.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        pk = self.kwargs['pk']
-        direc = self.kwargs['direc']
+        pk = self.kwargs["pk"]
+        direc = self.kwargs["direc"]
         # imageをpillowで読み込み、回転、保存する。
         qs = File.objects.get(pk=pk)
         img_path = qs.img.path
@@ -92,5 +98,5 @@ class FileRotateView(PermissionRequiredMixin, generic.TemplateView):
             img_rotate = img.rotate(90, expand=True)
         img_rotate.save(img_path)
         qs = File.objects.get(pk=pk)
-        context['img'] = qs.img.url
+        context["img"] = qs.img.url
         return context
